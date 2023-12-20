b1fcec56a514625e75ad99b8083e3d5f2eeff00d
shirleenma <shma4@s.sfusd.edu>
2023-12-20 11:07:14 -0800

    Player Scene FR completed
    i made my last commit too early sorry :c

---
+++ player.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var Speed = 400
+
+
+# Called when the node enters the scene tree for the first time.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
